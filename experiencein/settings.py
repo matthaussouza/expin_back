cbda476fa36edfe9ee8010dce684905ac4d0b12a
matthaussouza <71097104+matthaussouza@users.noreply.github.com>
2022-02-01 14:51:55 -0300

    Atualizando o Back

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES' : [
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
