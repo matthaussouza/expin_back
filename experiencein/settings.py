3cffa3ffcba7b40c086766468d83f3fa3c46b6e4
matthaussouza <71097104+matthaussouza@users.noreply.github.com>
2022-02-01 13:18:11 -0300

    Alterando o Front

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -108,8 +108,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES' : [
-        # 'rest_framework.authentication.TokenAuthentication'
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ]
 }
 
